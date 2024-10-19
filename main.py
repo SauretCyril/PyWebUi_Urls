c8825fe01cde88fa56cff073d0a9b463d36c7040
Cyril SAURET <sauretcyril@outlook.fr>
2024-10-19 21:37:13 +0200

    changement de port puis 1er déploiement

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from pathlib import Path
 
 # Add the parent directory of the current file to the Python path
 sys.path.append(str(Path(__file__).parent))
-import  functions 
+import functions 
 
+# Initialize Eel with the web directory
 eel.init("web")  # EEL initialization
 
-eel.start("main.html", size=(1000, 800))  # Starting the App 
+# Start the application on port 8080
+eel.start("main.html", size=(1000, 1000), port=8081)  # Starting the App
